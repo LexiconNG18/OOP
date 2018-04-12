7bec0d02d0d0abf9dcc017db0c20130f99e5ea7b
zano <adrian@xenotype.com>
2018-04-12 16:26:17 +0200

    Add project files.

---
+++ OOP/Classes.cs
@@ -0,0 +1,44 @@
+﻿namespace OOP
+{
+    interface Interface1
+    {
+        string I1();
+    }
+
+    interface Interface2
+    {
+        int I1();
+        string I2();
+    }
+
+    abstract class AbstractClass {
+        public abstract string Abstract();
+        public virtual string Concrete() => "Abstract.Concrete";
+    }
+
+    class AClass : AbstractClass, Interface1 {
+        public string I1() => "A.I1";
+        public override string Abstract() => "A.Abstract";
+        public string Hidden() => "A.Hidden"; 
+        public virtual string Override() => "A.Override";
+    }
+
+    sealed class BClass : AClass, Interface2 {
+        public string I2() => "B.I2";
+        public override string Concrete() => "B.Concrete";
+        public new string Hidden() => "B.Hidden is not " + base.Hidden();
+        public override string Override() => "B.Override";
+
+        // Explicit interface implementation
+        // - does not let you specify any access modifiers. 
+        // - explicitly implemented interface member can be  
+        //   accessed only using that interface.       
+        int Interface2.I1() => 10;
+    }
+
+    class CClass : Interface2
+    {
+        public int I1() => 11;
+        public string I2() => "C.I2";
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AClass a = new AClass();
+            AClass b = new BClass();
+            
+            Console.WriteLine(a.Override());
+            Console.WriteLine(b.Override());
+            Console.WriteLine();
+
+            Console.WriteLine(a.Hidden());
+            Console.WriteLine(b.Hidden());
+            Console.WriteLine(((BClass)b).Hidden());
+            Console.WriteLine();
+
+            Console.WriteLine(a.Abstract());
+            Console.WriteLine(b.Abstract());
+            Console.WriteLine();
+
+            Console.WriteLine(a.Concrete());
+            Console.WriteLine(b.Concrete());
+            Console.WriteLine();
+
+            CClass c = new CClass();
+
+            Console.WriteLine(a.I1());
+            Console.WriteLine(b.I1());
+            Console.WriteLine(((Interface2)b).I1());
+            Console.WriteLine(c.I1());
+            Console.WriteLine();
+
+            Console.WriteLine(((BClass)b).I2());
+            Console.WriteLine(c.I2());
+            Console.WriteLine();
+        }
+    }
+}
